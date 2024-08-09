27ec404c84244311d92dee9f7997a146b3a8f1da
SheilaNS <shei.nsantos@gmail.com>
2024-08-08 23:11:26 -0300

    Estruturas condicionais: condicional ternária

---
+++ estruturas-condicionais/src/ResultadoEscolarTernario.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolarTernario {
+  public static void main(String[] args) {
+    // Cenário 1
+    int nota1 = 7;
+
+    String resultado1 = nota1 >= 7 ? "Aprovado" : "Reprovado";
+
+    System.out.println(resultado1);
+
+    // Cenário 2
+    int nota2 = 6;
+
+    String resultado2 = nota2 >= 7 ? "Aprovado" : nota2 >= 5 && nota2 < 7 ? "Recuperação" : "Reprovado";
+
+    System.out.println(resultado2);
+  }
+}
