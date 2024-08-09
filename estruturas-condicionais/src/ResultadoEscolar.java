fa55747f548ae57f56ae2b98d2e1dd513bdc7cb3
SheilaNS <shei.nsantos@gmail.com>
2024-08-08 22:45:40 -0300

    Estruturas condicionais: condicional composta

---
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,21 @@
+public class ResultadoEscolar {
+  public static void main(String[] args) {
+    int nota = 6;
+
+    if (nota >= 7) {
+      System.out.println("Aprovado");
+    } else {
+      System.out.println("Reprovado");
+    }
+
+    /*
+     * Outra forma de escrever um if - else simples:
+     * 
+     * if(nota >= 7)
+     *  System.out.println("Aprovado");
+     * else
+     *  System.out.println("Reprovado");
+     */
+
+  }
+}
